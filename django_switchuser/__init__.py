efdfef519950277833a1162ea9c3408913381cb8
Luke Greenberg <51336686+openbracketret@users.noreply.github.com>
2025-10-22 13:56:11 +0200

    Update __init__.py
    course correction

--- django_switchuser/__init__.py
+++ django_switchuser/__init__.py
@@ -1,7 +1,3 @@
-# def version2str(ver):
-#     ver = map(str, ver)
-#     return ".".join(ver[:3]) + "".join(ver[3:])
 
- __version__ = (0, 6, 3)
-
- version_str = "0.6.3"
+__version__ = (0, 6, 3)
+version_str = "0.6.3"
